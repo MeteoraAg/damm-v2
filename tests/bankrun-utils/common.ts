190b82be409effb1326b0a8ef917e7f90ca3afdb
defi0x1 <cristiando0902@gmail.com>
2025-08-01 13:04:35 +0700

    feat: permissionless transfer hook

--- tests/bankrun-utils/common.ts
+++ tests/bankrun-utils/common.ts
@@ -8,6 +8,7 @@ import {
 import { BanksClient, ProgramTestContext, startAnchor } from "solana-bankrun";
 import { CP_AMM_PROGRAM_ID } from "./constants";
 import BN from "bn.js";
+import { TRANSFER_HOOK_COUNTER_PROGRAM_ID } from "./transferHook";
 
 export async function startTest(root: Keypair) {
   // Program name need to match fixtures program name
@@ -17,6 +18,10 @@ export async function startTest(root: Keypair) {
       {
         name: "cp_amm",
         programId: new PublicKey(CP_AMM_PROGRAM_ID),
+      },
+      {
+        name: "transfer_hook_counter",
+        programId: TRANSFER_HOOK_COUNTER_PROGRAM_ID,
       },
     ],
     [
