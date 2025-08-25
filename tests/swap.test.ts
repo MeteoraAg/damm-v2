3e8cddc469e908e3decb4a4c5153251c75c0de73
Arrowana <Arrowana@users.noreply.github.com>
2025-08-25 10:55:01 +1000

    fix token2022

--- tests/swap.test.ts
+++ tests/swap.test.ts
@@ -167,159 +167,159 @@ describe("Swap token", () => {
     });
   });
 
-  // describe("Token 2022", () => {
-  //   let context: ProgramTestContext;
-  //   let admin: Keypair;
-  //   let user: Keypair;
-  //   let creator: Keypair;
-  //   let config: PublicKey;
-  //   let liquidity: BN;
-  //   let sqrtPrice: BN;
-  //   let pool: PublicKey;
-  //   let position: PublicKey;
+  describe("Token 2022", () => {
+    let context: ProgramTestContext;
+    let admin: Keypair;
+    let user: Keypair;
+    let creator: Keypair;
+    let config: PublicKey;
+    let liquidity: BN;
+    let sqrtPrice: BN;
+    let pool: PublicKey;
+    let position: PublicKey;
 
-  //   let inputTokenMint: PublicKey;
-  //   let outputTokenMint: PublicKey;
+    let inputTokenMint: PublicKey;
+    let outputTokenMint: PublicKey;
 
-  //   beforeEach(async () => {
-  //     const root = Keypair.generate();
-  //     context = await startTest(root);
+    beforeEach(async () => {
+      const root = Keypair.generate();
+      context = await startTest(root);
 
-  //     const inputTokenMintKeypair = Keypair.generate();
-  //     const outputTokenMintKeypair = Keypair.generate();
-  //     inputTokenMint = inputTokenMintKeypair.publicKey;
-  //     outputTokenMint = outputTokenMintKeypair.publicKey;
+      const inputTokenMintKeypair = Keypair.generate();
+      const outputTokenMintKeypair = Keypair.generate();
+      inputTokenMint = inputTokenMintKeypair.publicKey;
+      outputTokenMint = outputTokenMintKeypair.publicKey;
 
-  //     const inputMintExtension = [
-  //       createTransferFeeExtensionWithInstruction(inputTokenMint),
-  //     ];
-  //     const outputMintExtension = [
-  //       createTransferFeeExtensionWithInstruction(outputTokenMint),
-  //     ];
-  //     const extensions = [...inputMintExtension, ...outputMintExtension];
-  //     user = await generateKpAndFund(context.banksClient, context.payer);
-  //     admin = await generateKpAndFund(context.banksClient, context.payer);
-  //     creator = await generateKpAndFund(context.banksClient, context.payer);
+      const inputMintExtension = [
+        createTransferFeeExtensionWithInstruction(inputTokenMint),
+      ];
+      const outputMintExtension = [
+        createTransferFeeExtensionWithInstruction(outputTokenMint),
+      ];
+      const extensions = [...inputMintExtension, ...outputMintExtension];
+      user = await generateKpAndFund(context.banksClient, context.payer);
+      admin = await generateKpAndFund(context.banksClient, context.payer);
+      creator = await generateKpAndFund(context.banksClient, context.payer);
 
-  //     await createToken2022(
-  //       context.banksClient,
-  //       context.payer,
-  //       inputMintExtension,
-  //       inputTokenMintKeypair
-  //     );
-  //     await createToken2022(
-  //       context.banksClient,
-  //       context.payer,
-  //       outputMintExtension,
-  //       outputTokenMintKeypair
-  //     );
+      await createToken2022(
+        context.banksClient,
+        context.payer,
+        inputMintExtension,
+        inputTokenMintKeypair
+      );
+      await createToken2022(
+        context.banksClient,
+        context.payer,
+        outputMintExtension,
+        outputTokenMintKeypair
+      );
 
-  //     await mintToToken2022(
-  //       context.banksClient,
-  //       context.payer,
-  //       inputTokenMint,
-  //       context.payer,
-  //       user.publicKey
-  //     );
+      await mintToToken2022(
+        context.banksClient,
+        context.payer,
+        inputTokenMint,
+        context.payer,
+        user.publicKey
+      );
 
-  //     await mintToToken2022(
-  //       context.banksClient,
-  //       context.payer,
-  //       outputTokenMint,
-  //       context.payer,
-  //       user.publicKey
-  //     );
+      await mintToToken2022(
+        context.banksClient,
+        context.payer,
+        outputTokenMint,
+        context.payer,
+        user.publicKey
+      );
 
-  //     await mintToToken2022(
-  //       context.banksClient,
-  //       context.payer,
-  //       inputTokenMint,
-  //       context.payer,
-  //       creator.publicKey
-  //     );
+      await mintToToken2022(
+        context.banksClient,
+        context.payer,
+        inputTokenMint,
+        context.payer,
+        creator.publicKey
+      );
 
-  //     await mintToToken2022(
-  //       context.banksClient,
-  //       context.payer,
-  //       outputTokenMint,
-  //       context.payer,
-  //       creator.publicKey
-  //     );
+      await mintToToken2022(
+        context.banksClient,
+        context.payer,
+        outputTokenMint,
+        context.payer,
+        creator.publicKey
+      );
 
-  //     // create config
-  //     const createConfigParams: CreateConfigParams = {
-  //       poolFees: {
-  //         baseFee: {
-  //           cliffFeeNumerator: new BN(2_500_000),
-  //           numberOfPeriod: 0,
-  //           reductionFactor: new BN(0),
-  //           periodFrequency: new BN(0),
-  //           feeSchedulerMode: 0,
-  //         },
-  //         padding: [],
-  //         dynamicFee: null,
-  //       },
-  //       sqrtMinPrice: new BN(MIN_SQRT_PRICE),
-  //       sqrtMaxPrice: new BN(MAX_SQRT_PRICE),
-  //       vaultConfigKey: PublicKey.default,
-  //       poolCreatorAuthority: PublicKey.default,
-  //       activationType: 0,
-  //       collectFeeMode: 0,
-  //     };
+      // create config
+      const createConfigParams: CreateConfigParams = {
+        poolFees: {
+          baseFee: {
+            cliffFeeNumerator: new BN(2_500_000),
+            numberOfPeriod: 0,
+            reductionFactor: new BN(0),
+            periodFrequency: new BN(0),
+            feeSchedulerMode: 0,
+          },
+          padding: [],
+          dynamicFee: null,
+        },
+        sqrtMinPrice: new BN(MIN_SQRT_PRICE),
+        sqrtMaxPrice: new BN(MAX_SQRT_PRICE),
+        vaultConfigKey: PublicKey.default,
+        poolCreatorAuthority: PublicKey.default,
+        activationType: 0,
+        collectFeeMode: 0,
+      };
 
-  //     config = await createConfigIx(
-  //       context.banksClient,
-  //       admin,
-  //       new BN(randomID()),
-  //       createConfigParams
-  //     );
+      config = await createConfigIx(
+        context.banksClient,
+        admin,
+        new BN(randomID()),
+        createConfigParams
+      );
 
-  //     liquidity = new BN(MIN_LP_AMOUNT);
-  //     sqrtPrice = new BN(MIN_SQRT_PRICE.muln(2));
+      liquidity = new BN(MIN_LP_AMOUNT);
+      sqrtPrice = new BN(MIN_SQRT_PRICE.muln(2));
 
-  //     const initPoolParams: InitializePoolParams = {
-  //       payer: creator,
-  //       creator: creator.publicKey,
-  //       config,
-  //       tokenAMint: inputTokenMint,
-  //       tokenBMint: outputTokenMint,
-  //       liquidity,
-  //       sqrtPrice,
-  //       activationPoint: null,
-  //     };
+      const initPoolParams: InitializePoolParams = {
+        payer: creator,
+        creator: creator.publicKey,
+        config,
+        tokenAMint: inputTokenMint,
+        tokenBMint: outputTokenMint,
+        liquidity,
+        sqrtPrice,
+        activationPoint: null,
+      };
 
-  //     const result = await initializePool(context.banksClient, initPoolParams);
-  //     pool = result.pool;
-  //     position = await createPosition(
-  //       context.banksClient,
-  //       user,
-  //       user.publicKey,
-  //       pool
-  //     );
-  //   });
+      const result = await initializePool(context.banksClient, initPoolParams);
+      pool = result.pool;
+      position = await createPosition(
+        context.banksClient,
+        user,
+        user.publicKey,
+        pool
+      );
+    });
 
-  //   it("User swap A->B", async () => {
-  //     const addLiquidityParams: AddLiquidityParams = {
-  //       owner: user,
-  //       pool,
-  //       position,
-  //       liquidityDelta: new BN(MIN_SQRT_PRICE.muln(30)),
-  //       tokenAAmountThreshold: new BN(200),
-  //       tokenBAmountThreshold: new BN(200),
-  //     };
-  //     await addLiquidity(context.banksClient, addLiquidityParams);
+    it("User swap A->B", async () => {
+      const addLiquidityParams: AddLiquidityParams = {
+        owner: user,
+        pool,
+        position,
+        liquidityDelta: new BN(MIN_SQRT_PRICE.muln(30)),
+        tokenAAmountThreshold: new BN(200),
+        tokenBAmountThreshold: new BN(200),
+      };
+      await addLiquidity(context.banksClient, addLiquidityParams);
 
-  //     const swapParams: SwapParams = {
-  //       payer: user,
-  //       pool,
-  //       inputTokenMint,
-  //       outputTokenMint,
-  //       amountIn: new BN(10),
-  //       minimumAmountOut: new BN(0),
-  //       referralTokenAccount: null,
-  //     };
+      const swapParams: SwapParams = {
+        payer: user,
+        pool,
+        inputTokenMint,
+        outputTokenMint,
+        amountIn: new BN(10),
+        minimumAmountOut: new BN(0),
+        referralTokenAccount: null,
+      };
 
-  //     await swap(context.banksClient, swapParams);
-  //   });
-  // });
+      await swap(context.banksClient, swapParams);
+    });
+  });
 });
