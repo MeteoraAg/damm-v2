0ea34d2805108a65b1f97afa4c9ba7f5279f875b
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-09-21 11:06:48 +0700

    update logic

--- programs/cp-amm/src/lib.rs
+++ programs/cp-amm/src/lib.rs
@@ -236,10 +236,17 @@ pub mod cp_amm {
         instructions::handle_split_position2(ctx, params.get_split_position_parameters2()?)
     }
 
-    pub fn split_position2(
-        ctx: Context<SplitPositionCtx>,
-        params: SplitPositionParameters2,
-    ) -> Result<()> {
-        instructions::handle_split_position2(ctx, params)
+    pub fn split_position2(ctx: Context<SplitPositionCtx>, numerator: u32) -> Result<()> {
+        instructions::handle_split_position2(
+            ctx,
+            SplitPositionParameters2 {
+                unlocked_liquidity_numerator: numerator,
+                permanent_locked_liquidity_numerator: numerator,
+                fee_a_numerator: numerator,
+                fee_b_numerator: numerator,
+                reward_0_numerator: numerator,
+                reward_1_numerator: numerator,
+            },
+        )
     }
 }
