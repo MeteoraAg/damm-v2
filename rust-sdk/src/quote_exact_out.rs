c918cb8023dc01cb01a84c322bb94128cd0abfc8
bangyro <229454856+bangyro@users.noreply.github.com>
2026-04-21 15:02:08 +0800

    fix: rust-sdk get nextSqrtPrice for compounding fee (#203)
    * fix: get nextSqrtPrice for compounding fee
    
    * fix: remove redundant CollectFeeMode parsing
    
    * feat: add test

--- rust-sdk/src/quote_exact_out.rs
+++ rust-sdk/src/quote_exact_out.rs
@@ -27,11 +27,19 @@ pub fn get_quote(
         .map_err(|_| Error::msg("Invalid collect fee mode"))?;
     let fee_mode = FeeMode::get_fee_mode(collect_fee_mode, trade_direction, has_referral);
 
-    let swap_result = pool.get_swap_result_from_exact_output(
+    let mut swap_result = pool.get_swap_result_from_exact_output(
         actual_amount_out,
         &fee_mode,
         trade_direction,
         current_point,
+    )?;
+
+    swap_result.next_sqrt_price = apply_next_sqrt_price(
+        pool,
+        &swap_result,
+        &fee_mode,
+        trade_direction,
+        collect_fee_mode,
     )?;
 
     Ok(swap_result)
--- rust-sdk/src/tests/mod.rs
+++ rust-sdk/src/tests/mod.rs
@@ -4,8 +4,14 @@ pub mod test_quote_exact_in;
 pub mod test_quote_exact_out;
 pub mod test_quote_partial_fill_in;
 
-use cp_amm::state::Pool;
+use cp_amm::{
+    get_initial_pool_information,
+    state::{CollectFeeMode, Pool},
+    InitialPoolInformation,
+};
 use std::fs;
+
+use crate::calculate_initial_sqrt_price::calculate_compounding_initial_sqrt_price_and_liquidity;
 
 pub const MACK_USDC_ADDRESS: &str = "3u2BK3ykdjv1hAeGQwAkZMxjb4otV5yvW7g72uviCaZZ";
 pub const SOL_USDC_CL_ADDRESS: &str = "CGPxT5d1uf9a8cKVJuZaJAU76t2EfLGbTmRbfvLLZp5j";
@@ -18,4 +24,27 @@ fn get_pool_account(pool_address: &str) -> Pool {
     let &pool: &Pool = bytemuck::from_bytes(&mut data_without_discriminator);
 
     pool
+}
+
+fn get_compounding_pool(token_a_amount: u64, token_b_amount: u64) -> Pool {
+    let (sqrt_price, liquidity) =
+        calculate_compounding_initial_sqrt_price_and_liquidity(token_a_amount, token_b_amount)
+            .expect("Failed to calculate initial sqrt price and liquidity");
+
+    let InitialPoolInformation {
+        token_a_amount,
+        token_b_amount,
+        sqrt_price,
+        ..
+    } = get_initial_pool_information(CollectFeeMode::Compounding, 0, 0, sqrt_price, liquidity)
+        .expect("Failed to get initial pool information");
+
+    Pool {
+        collect_fee_mode: CollectFeeMode::Compounding.into(),
+        token_a_amount,
+        token_b_amount,
+        liquidity,
+        sqrt_price,
+        ..Default::default()
+    }
 }
